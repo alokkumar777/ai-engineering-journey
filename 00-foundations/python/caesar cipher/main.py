fddbfa574a15abc50705e689cc52245c729733de
Alok Kumar <136910791+alokkumar777@users.noreply.github.com>
2026-01-05 22:42:17 +0530

    Build a Caesar Cipher

--- 00-foundations/python/caesar cipher/main.py
+++ 00-foundations/python/caesar cipher/main.py
@@ -1,7 +1,27 @@
-text = 'hello'
-shift = 3
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-shifted_alpha = alphabet[shift:] + alphabet[:shift]
-translation_table = str.maketrans(alphabet,shifted_alpha)
-encrypted_text = text.translate(translation_table)
-print(encrypted_text)   
+def caesar(text, shift, encrypt=True):
+  if not isinstance(shift, int):
+    return 'Shift should be an integer'
+  
+  if shift < 1 or shift > 25:
+    return 'Shift should be in between 1 to 25'
+  
+  if not encrypt:
+    shift = -shift
+
+  alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  shifted_alpha = alphabet[shift:] + alphabet[:shift]
+  translation_table = str.maketrans(alphabet + alphabet.upper(),shifted_alpha + shifted_alpha.upper())
+  encrypted_text = text.translate(translation_table)
+  return encrypted_text
+
+def encrypt(text, shift):
+  return caesar(text, shift)
+
+def decrypt(text, shift):
+  return caesar(text, shift, False)
+
+encrypted_text = encrypt('SuperCoder', 3)
+print("Encrypt: ", encrypted_text)
+
+decrypted_text = decrypt('VxshuFrghu', 3)
+print("Decrypt: ", decrypted_text)
