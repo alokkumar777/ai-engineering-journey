372749e93b10191778061b86b99301b50b732807
Alok Kumar <136910791+alokkumar777@users.noreply.github.com>
2026-01-31 18:33:17 +0530

    update

--- 00-foundations/python/medical data validator/main.py
+++ 00-foundations/python/medical data validator/main.py
@@ -39,3 +39,13 @@ def validate(data):
         print('Invalid format: expected a list or tuple.')
         return False
     
+    is_invalid = False
+
+    for index, dictionary in enumerate(data):
+        if not isinstance(dictionary, dict):
+            print(f'Invalid format: expected a dictionary at position {index}.')
+            is_invalid = True
+
+    if is_invalid:
+        return False
+
